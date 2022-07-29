5f2edb83ac77396ded1829bdabca52b50f993fd9
Maximilian Skoglund Lartell <max.lartell@gmail.com>
2022-07-29 14:15:53 +0200

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # FE-GQL-Boilerplate
+
+Todo
+
+- Add styled components
+- Add state management (redux or similiar)
+- Add graphql
--- fe-gql-boilerplate/src/components/RandomComponent/RandomComponent.test.tsx
+++ fe-gql-boilerplate/src/components/RandomComponent/RandomComponent.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
-import App from './RandomComponent'
+import RandomComponent from './RandomComponent'
 
 test('renders learn react link', () => {
-  render(<App />)
+  render(<RandomComponent />)
   const linkElement = screen.getByText(/learn react/i)
   expect(linkElement).toBeInTheDocument()
 })
